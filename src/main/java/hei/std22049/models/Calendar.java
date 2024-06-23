8859a2fe665f78d183958080e93a5ccbdcd71c62
Harenabs21 <hei.harena.6@gmail.com>
2024-06-23 17:03:06 +0300

    feat: create isHoliday method

--- src/main/java/hei/std22049/models/Calendar.java
+++ src/main/java/hei/std22049/models/Calendar.java
@@ -1,16 +1,22 @@
 package hei.std22049.models;
 
+import lombok.Getter;
+
 import java.time.LocalDate;
 import java.util.HashSet;
 import java.util.Set;
 
+@Getter
 public class Calendar{
     private Set<LocalDate> holidays;
 
     public Calendar(){
         this.holidays = new HashSet<>();
     }
-    public void ajouterJoursFeries(LocalDate date){
+    public void addHoliday(LocalDate date){
         holidays.add(date);
+    }
+    public boolean isHoliday(LocalDate date){
+        return holidays.contains(date);
     }
 }
