a99547a02d77054e9f0d1bceb10786a6927fd5aa
Harenabs21 <hei.harena.6@gmail.com>
2024-06-23 17:02:06 +0300

    chore: remove getter

--- src/main/java/hei/std22049/models/Category.java
+++ src/main/java/hei/std22049/models/Category.java
@@ -1,8 +1,5 @@
 package hei.std22049.models;
 
 import hei.std22049.models.utils.CategoryType;
-import lombok.Getter;
-
-@Getter
 public record Category(CategoryType name, int hoursPerWeek, double salaryPerWeek) {
 }
